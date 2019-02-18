355c658fc786ab6f446cbb252382a2c84865e54d
Flounder (PC) <jmmyers1@student.fullsail.edu>
2019-02-18 12:03:49 -0500

    Reconfigured the organization to make the repository more usable.

--- Enviorment/Active/Misc/Mantainance/savenamelist.py
+++
@@ -1,8 +0,0 @@
-from void_scribe import NamesDictionary
-
-nd = NamesDictionary.NamesDictionary()
-names = nd.keys()
-
-with open('name_type_list.txt',"w+") as f:
-    for name in names:
-        f.write(name + '\n')
--- Enviorment/Active/Misc/Utility/generateTextNameLists.py
+++
@@ -1,7 +0,0 @@
-import os
-from void_scribe import NameGenerator
-
-f = open("namelist.txt", "a")
-
-for name in list(NameGenerator.getNameTypes()):
-    f.write(str(name)+'\n')
